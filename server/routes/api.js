c8cf86ddb67ece2370722c7692522dc08216e966
James Freeman <freeman.james.h@gmail.com>
2016-05-13 16:30:35 -0600

    server connected to database

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({title: 'Express'});
+  knex('users').then(function(users){
+    res.json(users)
+  })
 });
 
 module.exports = router;
