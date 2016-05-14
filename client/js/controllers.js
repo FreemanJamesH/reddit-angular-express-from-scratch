6eecc7026aaa1eca523da33935fed1a7b21bfb27
James Freeman <freeman.james.h@gmail.com>
2016-05-14 14:03:27 -0600

    more housekeeping

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,9 +1,11 @@
 app.controller('MainController', function($scope, dbService, $http){
 
+$scope.vm = {};
+
 // its good to set the variable in the .then of the function so that the data will be available by the time you set the variable
  dbService.getPeeps().then(function(response){
     console.log('response in controller', response);
-   $scope.message = response.data;
+   $scope.vm.message = response.data;
  })
 
 })
