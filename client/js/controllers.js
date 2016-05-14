75e2b9994e11eb0ff3d2da5ca40a3f6b401c2809
James Freeman <freeman.james.h@gmail.com>
2016-05-14 14:00:23 -0600

    housekeeping

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,6 +1,6 @@
 app.controller('MainController', function($scope, dbService, $http){
 
-// its good to set the variable in the .then of the function so that the data will be available by the time you set the variablewre
+// its good to set the variable in the .then of the function so that the data will be available by the time you set the variable
  dbService.getPeeps().then(function(response){
     console.log('response in controller', response);
    $scope.message = response.data;
--- client/js/services.js
+++ client/js/services.js
@@ -1,13 +1,8 @@
 app.service('dbService', function($http){
 
-// var dbService = {}
-
  this.getPeeps = function(){
-
-  return $http.get('http://localhost:3000/');
-
+   return $http.get('http://localhost:3000/');
 }
 
-// return dbService
 
 })
