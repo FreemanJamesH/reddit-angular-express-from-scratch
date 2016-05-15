1c6bf7d7ebd835cc8e5ab08c1c23731d368cdb1e
James Freeman <freeman.james.h@gmail.com>
2016-05-15 16:21:23 -0600

    housekeeping

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,18 +1,24 @@
-app.controller('MainController', function($scope, dbService, $http){
+app.controller('MainController', function($scope, dbService, $http) {
 
-$scope.vm = {};
+  $scope.vm = {};
 
-// its good to set the variable in the .then of the function so that the data will be available by the time you set the variable
- dbService.getPosts().then(function(response){
+  // its good to set the variable in the .then of the function so that the data will be available by the time you set the variable
+  dbService.getPosts().then(function(response) {
     console.log('response in controller', response);
-   $scope.vm.posts = response.data;
- })
+    $scope.vm.posts = response.data;
+  })
 
 
-$scope.submit = function(title, author, image, description){
-   var obj = {title: title, user_id: 3, image_src: image, description:description}
-   console.log(dbService.submitPost(obj))
-
-}
+  $scope.submit = function(title, author, image, description) {
+    var obj = {
+      title: title,
+      user_id: 3,
+      image_src: image,
+      description: description
+    }
+    dbService.submitPost(obj).then(function(returnedData) {
+      $scope.vm.posts.push(returnedData)
+    })
+  }
 
 })
